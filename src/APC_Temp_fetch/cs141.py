41d0635118dbd2d6eeb3cc0ee967bcd0b3995e4c
zseri <zseri.devel@ytrizja.de>
2021-11-23 18:34:01 +0100

    cs141: use session as context manager

--- src/APC_Temp_fetch/cs141.py
+++ src/APC_Temp_fetch/cs141.py
@@ -3,20 +3,24 @@ from .base import ApcKind
 
 class Cs141(ApcKind):
     def fetch(self, user, password):
-        s = requests.Session()
-        r = self.urlway(0, 'http://' + host + '/api/login', lambda xurl: s.post(xurl, data = {
-            'userName': user,
-            'password': password,
-            'anonymous': '',
-            'newPassword': '',
-        }))
+        upsst = None
 
-        try:
-            r = self.urlway(1, 'http://' + host + '/api/devices/ups/report', lambda xurl: s.get(xurl))
-        finally:
-            self.urlway(2, 'http://' + host + '/api/logout', lambda xurl: s.post(xurl, data = { 'userName': user }))
+        with requests.Session() as s:
+            r = self.urlway(0, 'http://' + host + '/api/login', lambda xurl: s.post(xurl, data = {
+                'userName': user,
+                'password': password,
+                'anonymous': '',
+                'newPassword': '',
+            }))
 
-        upsst = r.json()['ups']['valtable']
+            try:
+                r = self.urlway(1, 'http://' + host + '/api/devices/ups/report', lambda xurl: s.get(xurl))
+            finally:
+                self.urlway(2, 'http://' + host + '/api/logout', lambda xurl: s.post(xurl, data = { 'userName': user }))
+
+            upsst = r.json()['ups']['valtable']
+            del r
+
         self.eprint(F'{host}: [result]:', repr(upsst))
         return upsst
 
