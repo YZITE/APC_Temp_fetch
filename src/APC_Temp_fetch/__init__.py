4faeb666b6a185a8f713cc6536560e48ec98ef69
zseri <zseri.devel@ytrizja.de>
2022-03-25 22:16:06 +0100

    also put version into __init__.py

--- src/APC_Temp_fetch/__init__.py
+++ src/APC_Temp_fetch/__init__.py
@@ -3,7 +3,8 @@ from .frmnc import Frmnc
 from .cs141 import Cs141
 from .gden_nt07 import GdenNt07
 
-__all__ = ['KINDS']
+__all__ = ['KINDS', '__version__']
+__version__ = '0.0.0'
 
 KINDS = {
     'old': Old,
